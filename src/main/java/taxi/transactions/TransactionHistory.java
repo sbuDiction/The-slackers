abd4cd06732da5824293922f3ce6c1889c6f9182
sbuDiction <nsbu68501@gmail.com>
2020-09-17 15:28:00 +0200

    late working

--- src/main/java/taxi/transactions/TransactionHistory.java
+++ src/main/java/taxi/transactions/TransactionHistory.java
@@ -1,13 +1,37 @@
 package taxi.transactions;
 
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class TransactionHistory {
 
+    int priceRef;
+    int daysRef;
+    String timeStamp;
 
-//    public static void main(String[] args) {
+    public void setPriceRef(int priceRef) {
+        this.priceRef = priceRef;
+    }
+
+    public void setDaysRef(int daysRef) {
+        this.daysRef = daysRef;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getPriceRef() {
+        return priceRef;
+    }
+
+    public int getDaysRef() {
+        return daysRef;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    //    public static void main(String[] args) {
 //        LocalDateTime time = LocalDateTime.now();
 //        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
 //
